1aa2aec04e1ff49c7cd5a6e68e9aee847d7db8e4
DESKTOP-A8PBUFL\jose.assis <jose.assis@mprj.mp.br>
2022-04-22 14:24:24 -0300

    Alteração no controller user e no resourceServerconfig

--- src/main/java/com/devsuperior/movieflix/config/ResourceServerConfig.java
+++ src/main/java/com/devsuperior/movieflix/config/ResourceServerConfig.java
@@ -5,7 +5,6 @@ import java.util.Arrays;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
@@ -23,9 +22,9 @@ public class ResourceServerConfig  extends ResourceServerConfigurerAdapter{
 	@Autowired
 	private JwtTokenStore tokenStore;
 	
-	private static final String[] PUBLIC = {"/oauth/token", "/h2-console/**"};
+	private static final String[] PUBLIC = {"/oauth/token", "/h2-console/**"}; 
 	
-	//private static final String[] CLIENT_GET = {"/cities/**", "/events/**"};
+	//private static final String[] CLIENT_GET = {"/cities/**", "/events/**"}; 
 	
 	//private static final String[] CLIENT_POST_EVENT = {"/events/**"};
 	
